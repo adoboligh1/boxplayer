d816c7dab1d2661b9dfa4d2f1c508d620a76e0cc
Albert Adoboli <43940776+adoboligh1@users.noreply.github.com>
2021-04-24 18:56:57 +0000

    Add files via upload

---
+++ classical/config.js
@@ -0,0 +1,12 @@
+var settings = {
+    'radio_name': 'Classical Radio',
+
+    'url_streaming': 'https://classical.boxradio.ghanacloud.ga',
+
+    'api_url': 'https://api.ghanacloud.ga/classical',
+  
+    
+  'api_key': '18fe07917957c289983464588aabddfb',
+
+    'default_cover_art': 'img/radio.png',
+};
